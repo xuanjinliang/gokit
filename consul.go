c4c76d10b110c8b6ce530a0b668b5870971fa2c1
xuanjinliang <xuanjinliang@gmail.com>
2020-11-11 17:52:08 +0800

    add consul

---
+++ consul.go
@@ -0,0 +1,63 @@
+package gotool
+
+import (
+	"errors"
+	"github.com/hashicorp/consul/api"
+)
+
+var ErrConsulKeyNotExist = errors.New("consul: key not exist")
+
+type Consul struct {
+	address string
+	health  *api.Health
+	kv      *api.KV
+}
+
+func NewConsul(address string) *Consul {
+	config := api.DefaultConfig()
+	config.Address = address
+	client, err := api.NewClient(config)
+	if err != nil {
+		panic("connect consul failed, err:" + err.Error())
+	}
+
+	consul := &Consul{
+		address: address,
+		health:  client.Health(),
+		kv:      client.KV(),
+	}
+	return consul
+}
+
+func (c *Consul) GetKey(key string) ([]byte, error) {
+	pair, _, err := c.kv.Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	if pair == nil {
+		return nil, ErrConsulKeyNotExist
+	}
+	return pair.Value, nil
+}
+
+func (c *Consul) GetService(service string) ([]*api.ServiceEntry, error) {
+	entry, _, err := c.health.Service(service, "", true, nil)
+	return entry, err
+}
+
+func (c *Consul) PutKey(key string, value []byte) error {
+	pair := &api.KVPair{Key: key, Value: value}
+	_, err := c.kv.Put(pair, nil)
+	return err
+}
+
+func (c *Consul) GetList(key string) (api.KVPairs, error) {
+	pair, _, err := c.kv.List(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	if len(pair) == 0 {
+		return nil, ErrConsulKeyNotExist
+	}
+	return pair, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/xuanjinliang/gotool
 go 1.15
 
 require (
+	github.com/hashicorp/consul/api v1.7.0 // indirect
 	github.com/olivere/elastic/v7 v7.0.22
 	github.com/sirupsen/logrus v1.7.0
 )
